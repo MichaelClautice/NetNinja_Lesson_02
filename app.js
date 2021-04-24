a46c65b483165a8e93f6c157bf8051c1efe4619a
Michael Clautice <michaelclautice@Michaels-MacBook-Pro-2.local>
2021-04-24 13:38:37 -0400

    Sat, Apr 24, 2021, Michael updated app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
 console.log('••••••••••••••••');
 console.log('•••Lesson_2•••');
 console.log('Hey Ninjas!');
-console.log('update test w7.29.20');
+console.log('Update test Sat, Apr 24, 2021');
 console.log('••••••••••••••••');
